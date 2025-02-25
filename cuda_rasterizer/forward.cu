20dddbd25ff2ecb05d276bf6adf71e34b67933e6
Kenneth Yang <kjy5@uw.edu>
2025-02-24 17:50:56 -0800

    Generate point reodering, but it's super slow

--- cuda_rasterizer/forward.cu
+++ cuda_rasterizer/forward.cu
@@ -308,11 +308,12 @@ renderCUDA(
 	uint2 range = ranges[block.group_index().y * horizontal_blocks + block.group_index().x];
 	const int rounds = ((range.y - range.x + BLOCK_SIZE - 1) / BLOCK_SIZE);
 	int toDo = range.y - range.x;
-#define MAX_TODO 1024
+#define MAX_TODO 2293
 
 	if (pix_id == 0) {
 		// Get the order of the points in the global list.
 		int global_order_index[MAX_TODO];
+		printf("Number of TODO: %d\n", toDo);
 		for (int i = 0; i < toDo; ++i) {
 			for (int j = 0; j < splat_id_count; ++j) {
 				if (global_splat_id_list[j] == point_list[range.x + i]) {
@@ -321,27 +322,33 @@ renderCUDA(
 				}
 			}
 		}
-
+		for (int i = 0; i < toDo; ++i) {
+			printf("Splat index %d has global index %d\n", range.x + i, global_order_index[i]);
+		}
+		
 		// Create a new list of points sorted by global order.
 		uint32_t sorted_point_list[MAX_TODO];
 		bool used[MAX_TODO] = { false };
-
+		
 		// Find and place points in order.
 		for (int i = 0; i < toDo; ++i) {
 			// Find the unused point with lowest global order.
 			int min_global_index = splat_id_count;
 			int min_local_index = -1;
-
+		
 			for (int j = 0; j < toDo; ++j) {
 				if (!used[j] && global_order_index[j] < min_global_index) {
 					min_global_index = global_order_index[j];
 					min_local_index = j;
 				}
 			}
-
+		
 			// Place the point in the sorted list.
 			sorted_point_list[i] = point_list[range.x + min_local_index];
 			used[min_local_index] = true;
+		}
+		for (int i = 0; i < toDo; ++i) {
+			printf("%d\n",sorted_point_list[i]);
 		}
 	}
 
